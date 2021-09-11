b6c7c4bb2cb3f8d618bb8eb452deccf58d8ce854
Artak-Ananyan <artaananyan@gmail.com>
2021-09-11 21:28:05 +0400

    --m

---
+++ lib/calculiatr.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class calculiatr extends StatefulWidget {
+  const calculiatr({Key? key}) : super(key: key);
+
+  @override
+  _calculiatrState createState() => _calculiatrState();
+}
+
+class _calculiatrState extends State<calculiatr> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 5),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Padding(
+              padding: EdgeInsets.all(10.0),
+              child: Text(
+                "0",
+                style: TextStyle(color: Colors.white),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
